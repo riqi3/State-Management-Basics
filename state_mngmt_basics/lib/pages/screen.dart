81b57b563b11afe230a7e2a463e6ff00548a3e65
riqi3 <enrique.pacudan@usjr.edu.ph>
2023-03-27 21:49:46 +0800

    contactbook

---
+++ state_mngmt_basics/lib/pages/screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class screen extends StatelessWidget {
+  const screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final contactBook = ContactBook();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home page'),
+      ),
+      backgroundColor: Colors.amber,
+      body: ListView.builder(
+        itemCount: contactBook.length,
+        itemBuilder: (context, index) {
+          final contact = contactBook.contact(atIndex: index)!;
+          return ListTile(
+            title: Text(
+              contact.name,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
